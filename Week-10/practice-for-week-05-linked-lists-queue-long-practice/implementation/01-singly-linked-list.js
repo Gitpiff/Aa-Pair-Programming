59b7890df2718c2f13edf1382d2494e3b2658bb4
Rafat <bellorafat1@gmail.com>
2023-08-08 20:04:14 -0400

    whatever again

--- Week-10/practice-for-week-05-linked-lists-queue-long-practice/implementation/01-singly-linked-list.js
+++ Week-10/practice-for-week-05-linked-lists-queue-long-practice/implementation/01-singly-linked-list.js
@@ -62,25 +62,48 @@ class SinglyLinkedList {
 
     removeFromTail() {
         // Remove node at tail
-        const removed = this.head.next
+        if(!this.head) {
+            return undefined
+        }
+        if (!this.head.next) {
+            const removed = this.head
+            this.head = null
+            this.length--
+            return removed
 
+        } else {
+            let newTail = this.head
+        while(newTail.next.next) {
+            newTail = newTail.next
+        }
+
+        const removed = newTail.next
+        newTail.next = null
+        
+        
         this.length --
         return removed
+        }
+        
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtHead() {
         // Return the value of head node
-
-        // Your code here
+        if (!this.head) return undefined;
+        return this.head.value
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     print() {
         // Print out the linked list
-
-        // Your code here
+      if (!this.head) return null;
+      let curr = this.head
+      while(curr) {
+        console.log(curr.value)
+        curr = curr.next
+      }
 
         // Write your hypothesis on the time complexity of this method here
     }
