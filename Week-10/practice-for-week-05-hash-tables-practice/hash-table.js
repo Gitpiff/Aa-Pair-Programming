575f725c8c46b26bb4dd2449d333b1c539684ccb
traceybee23 <tracebee23@gmail.com>
2023-08-09 19:17:28 -0500

    updates

--- Week-10/practice-for-week-05-hash-tables-practice/hash-table.js
+++ Week-10/practice-for-week-05-hash-tables-practice/hash-table.js
@@ -40,7 +40,18 @@ class HashTable {
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    let index = this.hashMod(key);
+
+    if(!this.data[index]) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      let node = new KeyValuePair(key, value);
+
+      node.next = this.head;
+      this.head = node;
+      this.count++;
+    }
   }
 
   insert(key, value) {
