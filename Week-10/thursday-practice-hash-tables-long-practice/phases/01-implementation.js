8eb405c11a2adbf477ea45ad76d411a27ea8b5ef
Victor <viknavarro@gmail.com>
2023-08-11 00:27:53 +0100

    01 read

--- Week-10/thursday-practice-hash-tables-long-practice/phases/01-implementation.js
+++ Week-10/thursday-practice-hash-tables-long-practice/phases/01-implementation.js
@@ -10,7 +10,9 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
     // Initialize your buckets here
-    // Your code here
+    this.count = 0
+    this.capacity = numBuckets
+    this.data = new Array(this.capacity).fill(null)
   }
 
   hash(key) {
@@ -30,12 +32,44 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    let index = this.hashMod(key) //Index to add at
+
+    let oldPair = this.data[index] //check if key exists
+
+    while(oldPair && oldPair.key !== key) { //while current node exists & it's key !== key  
+      oldPair = oldPair.next  //oldPair checks the next node
+    } //loop stops when oldPair === null or oldPair.key === key
+
+    if(oldPair){ //if there's an oldPair
+      oldPair.value = value  //reassing value
+    } else {
+      let newPair = new KeyValuePair(key, value) //make a new key value node
+      if(!this.data[index]) { //if spot is empty
+        this.data[index] = newPair //populate it with new node
+      } else {
+        newPair.next = this.data[index] //move oldPair to the .next index
+        this.data[index] = newPair //reassing current index to newPair 
+      }
+
+      this.count ++ //update count of list by one
+    }
   }
 
 
   read(key) {
-    // Your code here
+    let index = this.hashMod(key) //Index to read
+
+    let current = this.data[index] //Grab current node
+    //console.log(current)
+    while(current && current.key !== key) { //while current node exists & it's key !== key  
+      current = current.next  //current checks the next node
+    } //loop stops when current === null or current.key === key
+   if(current) { //there's a node that contains the key
+    return current.value //return the value of the key
+   } else { //if no node has a matching key return undefined
+    return undefined
+   }
+
   }
 
 
