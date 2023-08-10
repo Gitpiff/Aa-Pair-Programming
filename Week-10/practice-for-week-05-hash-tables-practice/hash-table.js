1152425f63607941529c531d4d4fad0a741008f4
Victor <viknavarro@gmail.com>
2023-08-10 01:56:27 +0100

    has-table

--- Week-10/practice-for-week-05-hash-tables-practice/hash-table.js
+++ Week-10/practice-for-week-05-hash-tables-practice/hash-table.js
@@ -44,15 +44,40 @@ class HashTable {
 
     let newNode = new KeyValuePair(key, value) //create new node
     console.log(this.data[index])
-    if(this.data[index]) {              
-      newNode.next = this.data[index]
+    if(this.data[index]) {       //Collision       
+      newNode.next = this.data[index] 
     }
-    this.data[index] = newNode                //store it in the data bucket location returned by hashMod(key)
+    this.data[index] = newNode     //store it in the data bucket location returned by hashMod(key)
     this.count ++
   }
 
   insert(key, value) {
-    // Your code here
+    let index = this.hashMod(key);
+    
+    let oldPair = this.data[index]
+
+    //Check if index and oldPair have the same key
+    //We're checking multiple nodes -linked list- use a while loop
+    while(oldPair && oldPair.key !== key) {
+      oldPair = oldPair.next
+    }
+
+    if(oldPair){ //if oldPair has the ley we're trying to push
+      oldPair.value = value
+    } else {
+      this.insertWithHashCollisions(key, value)
+    }
+    // else { //using different key but same value
+    //   let newPair = new KeyValuePair(key, value)
+    //   if(!this.data[index]) { //if there's something at this index
+    //     this.data[index] = newPair
+    //   } else {
+    //     newPair.next = this.data[index]
+    //     this.data[index] = newPair
+    //   }
+
+    //   this.count ++
+    // }
   }
 
 }
