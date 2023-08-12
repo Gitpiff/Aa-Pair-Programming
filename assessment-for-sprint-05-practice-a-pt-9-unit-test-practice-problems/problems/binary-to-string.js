f84edd59adc89ca0346c94c6cfd782271f5fb740
Victor <viknavarro@gmail.com>
2023-08-12 18:11:11 +0100

    practice assessment

--- assessment-for-sprint-05-practice-a-pt-9-unit-test-practice-problems/problems/binary-to-string.js
+++ assessment-for-sprint-05-practice-a-pt-9-unit-test-practice-problems/problems/binary-to-string.js
@@ -1,10 +1,19 @@
 function binaryToString(binaryBlob) {
-  // Your code here
+  let str = ""
+
+  for(let i  = 0; i < binaryBlob.length; i += 8) {
+    const eightBit = binaryBlob.slice(i, i + 8)
+    const decimal = parseInt(eightBit, 2)
+    const newString = String.fromCharCode(decimal)
+    str += newString
+  }
+
+  return str 
 }
 
 /* Comment in the code below to run local tests */
-// console.log(binaryToString('010000010100001001000011')); // 'ABC'
-// console.log(binaryToString('001101100011011100111000')); // '678'
+console.log(binaryToString('010000010100001001000011')); // 'ABC'
+console.log(binaryToString('001101100011011100111000')); // '678'
 
 
 module.exports = binaryToString;
