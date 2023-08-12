97874342e13538fe8df2da8fb6833037ba34a191
Victor <viknavarro@gmail.com>
2023-08-12 18:16:58 +0100

    practice assessment pt2

--- assessment-for-sprint-05-practice-a-pt-9-unit-test-practice-problems/problems/binary-to-string.js
+++ assessment-for-sprint-05-practice-a-pt-9-unit-test-practice-problems/problems/binary-to-string.js
@@ -1,10 +1,15 @@
 function binaryToString(binaryBlob) {
+  //Return a string
   let str = ""
-
+  //Loop over the "binaryBlob", but iterate every 8 chars
   for(let i  = 0; i < binaryBlob.length; i += 8) {
+    //Store the first 8 chars
     const eightBit = binaryBlob.slice(i, i + 8)
+    //Turn them into a decimal value
     const decimal = parseInt(eightBit, 2)
+    //Convert the decimal value into a string
     const newString = String.fromCharCode(decimal)
+    //Add the newly created string to the rest of the string 
     str += newString
   }
 
