805e631e7b466dca12abf80f225847e18bb8f65e
Victor <viknavarro@gmail.com>
2023-08-26 19:57:49 +0100

    practice assesssment 2

--- Week-12/assessment-for-sprint-06-practice-pt-12-graphs-hiking-mountains/hiking-mountains.js
+++ Week-12/assessment-for-sprint-06-practice-pt-12-graphs-hiking-mountains/hiking-mountains.js
@@ -44,67 +44,143 @@ function findStarts(matrix) {
 
     return starts;
 }
-
+                    // r c
 function findNeighbors(node, matrix) {
     // Don't forget to include diagonal neighbors!!!
+    const neighbors = []
+    const [row, col] =  node
+    const rows = matrix.length
+    const cols = matrix[0].length
 
-    // Your code here
+    const currValue = matrix[row][col]
+    
+
+    //Top Left                  Curr Value                  One less
+    if(row > 0 && col > 0 && Math.abs(currValue - matrix[row -1][col -1]) <= 1) {
+        neighbors.push([row - 1, col - 1])
+    }
+
+    //Top                          
+    if(row > 0 && Math.abs(currValue - matrix[row -1][col]) <= 1) {
+        neighbors.push([row - 1, col])
+    }
+
+    //Top Right
+    if(row > 0 && col < cols - 1 && Math.abs(currValue - matrix[row -1][col + 1]) <= 1) {
+        neighbors.push([row - 1, col + 1])
+    }
+
+    //Right
+    if(col < cols - 1 && Math.abs(currValue - matrix[row][col + 1]) <= 1) {
+        neighbors.push([row, col + 1])
+    }
+
+    //Bottom Right
+    if(row < rows - 1 && col < cols - 1 && Math.abs(currValue - matrix[row + 1][col + 1]) <= 1) {
+        neighbors.push([row + 1, col + 1])
+    }
+
+    //Bottom
+    if(row < rows - 1 && Math.abs(currValue - matrix[row + 1][col]) <= 1) {
+        neighbors.push([row + 1, col])
+    }
+
+    //Bottom Left
+    if(row < rows - 1 && col > 0 && Math.abs(currValue - matrix[row + 1][col - 1]) <= 1) {
+        neighbors.push([row + 1, col - 1])
+    }
+
+    //Left
+    if(col > 0 && Math.abs(currValue - matrix[row][col - 1]) <= 1) {
+        neighbors.push([row, col - 1])
+    }
+
+    return neighbors
 }
 
 function pathTraversal(node, matrix, visited, peak) {
-    // Your code here
+    const queue = [node]
+    visited.add(node.toString())
+
+    while(queue.length) {
+        const currPeak = queue.shift()
+        const row = currPeak[0]
+        const col = currPeak[1]
+        
+        if(matrix[row][col] === peak) return true
+        //console.log(currPeak)
+        const neighbors = findNeighbors(currPeak, matrix)
+
+        for(let neighbor of neighbors) {
+            if(!visited.has(neighbor.toString())) {
+                visited.add(neighbor.toString())
+                queue.push(neighbor)
+            }
+        }
+    }
+    
+    return false
 }
 
 function identifyPath(mountain) {
     // Find the peak
+    const peak = findPeak(mountain)
     // Find the start
-
+    const start = findStarts(mountain)
+    const visited = new Set()
     // Traverse from the starts and try to get to the top
-    // Your code here
+    while(start.length) {
+        const currMountain = start.shift()
+        const path = pathTraversal(currMountain, mountain, visited, peak)
+        if(path) {
+            return currMountain
+        }
+    }
+    
 }
 
 // Uncomment for local testing
 
-// // Example 0
-// const mountain_0 = [
-//     [1, 2, 4],
-//     [4, 5, 9],
-//     [5, 7, 6]
-// ];
+// Example 0
+const mountain_0 = [
+    [1, 2, 4],
+    [4, 5, 9],
+    [5, 7, 6]
+];
 
-// console.log(findNeighbors([2,0], mountain_0)) // <- Expect '[ [ 1, 0 ], [ 1, 1 ] ]'
+console.log(findNeighbors([2,0], mountain_0)) // <- Expect '[ [ 1, 0 ], [ 1, 1 ] ]'
 
-// // Example 1
-// const mountain_1 = [
-//         [1, 0, 1, 1],
-//         [2, 3, 2, 1],
-//         [0, 2, 4, 1],
-//         [3, 2, 3, 1]
-// ];
+// Example 1
+const mountain_1 = [
+        [1, 0, 1, 1],
+        [2, 3, 2, 1],
+        [0, 2, 4, 1],
+        [3, 2, 3, 1]
+];
 
-// test_visited = new Set()
-// console.log(pathTraversal([0, 1], mountain_1, test_visited, 4)) // <- Expect 'true
-// console.log(identifyPath(mountain_1)) // <- Expect '[ 0, 1 ]'
+test_visited = new Set()
+console.log(pathTraversal([0, 1], mountain_1, test_visited, 4)) // <- Expect 'true
+console.log(identifyPath(mountain_1)) // <- Expect '[ 0, 1 ]'
 
-// // Example 2
-// const mountain_2 = [
-//         [0, 2, 1, 1],
-//         [2, 2, 3, 1],
-//         [1, 1, 1, 1],
-//         [1, 0, 1, 1]
-// ];
+// Example 2
+const mountain_2 = [
+        [0, 2, 1, 1],
+        [2, 2, 3, 1],
+        [1, 1, 1, 1],
+        [1, 0, 1, 1]
+];
 
-// console.log(identifyPath(mountain_2)) // <- Expect '[ 3, 1 ]'
+console.log(identifyPath(mountain_2)) // <- Expect '[ 3, 1 ]'
 
-// // Example 3
-// const mountain_3 = [
-//         [0, 1, 2, 0],
-//         [5, 1, 3, 2],
-//         [4, 1, 2, 1],
-//         [3, 4, 3, 1]
-// ];
+// Example 3
+const mountain_3 = [
+        [0, 1, 2, 0],
+        [5, 1, 3, 2],
+        [4, 1, 2, 1],
+        [3, 4, 3, 1]
+];
 
-// console.log(identifyPath(mountain_3)) // <- Expect '[ 0, 0 ]'
+console.log(identifyPath(mountain_3)) // <- Expect '[ 0, 0 ]'
 
 
 
