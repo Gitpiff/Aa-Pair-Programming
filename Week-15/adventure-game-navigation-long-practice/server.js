6cca1a96cba905a96563ca17d093b8cd0138d16b
Carmen <carmenshiu07@gmail.com>
2023-09-16 15:15:39 -0700

    solved phase 2 and 3

--- Week-15/adventure-game-navigation-long-practice/server.js
+++ Week-15/adventure-game-navigation-long-practice/server.js
@@ -11,6 +11,11 @@ let world = new World();
 world.loadWorld(worldData);
 
 const server = http.createServer((req, res) => {
+  function redirectTo(urlPath) {
+    res.statusCode = 302;
+    res.setHeader('Location', urlPath);
+    return res.end();
+  }
 
   /* ============== ASSEMBLE THE REQUEST BODY AS A STRING =============== */
   let reqBody = '';
@@ -34,18 +39,51 @@ const server = http.createServer((req, res) => {
 
     /* ======================== ROUTE HANDLERS ========================== */
     // Phase 1: GET /
-    if(req.method === "GET" && req.url === "/") {
+    if (req.method === "GET" && req.url === "/") {
       const htmlFile = fs.readFileSync("./views/new-player.html", "utf-8")
 
       const resBody = htmlFile.replace(/#{availableRooms}/g, world.availableRoomsToString())
       res.statusCode = 200
       res.setHeader("Content-Type", "text/html")
-      return res.end(resBody)
+      res.write(resBody)
+      return res.end()
     }
 
     // Phase 2: POST /player
+    if (req.method === "POST" && req.url === "/player") {
+
+      const { name, roomId } = req.body
+
+      player = new Player(name, world.rooms[roomId])
+
+      res.statusCode = 200
+      return redirectTo(`/rooms/${roomId}`)
+    }
 
     // Phase 3: GET /rooms/:roomId
+    if (req.method === "GET" && req.url.startsWith("/rooms")) {
+      const urlParts = req.url.split("/");
+      const roomId = urlParts[2];
+      if (urlParts.length === 3 && roomId) {
+        if (roomId == player.currentRoom.id) {
+          const room = player.currentRoom;
+
+          const htmlFile = fs.readFileSync("./views/room.html", "utf-8");
+          let reqBody = htmlFile
+            .replace(/#{roomName}/g, room.name)
+            .replace(/#{roomItems}/g, room.itemsToString())
+            .replace(/#{inventory}/g, player.inventoryToString())
+            .replace(/#{exits}/g, room.exitsToString());
+
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/html");
+          res.write(reqBody);
+          return res.end();
+        } else {
+          return redirectTo(`/rooms/${player.currentRoom.id}`);
+        }
+      }
+    }
 
     // Phase 4: GET /rooms/:roomId/:direction
 
