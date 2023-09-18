1230259749158275a95f7069799c199ff5adbb51
Victor <viknavarro@gmail.com>
2023-09-18 23:51:43 +0100

    Promises

--- Mod-3/Week-16/create-and-handle-promises/phase-1.js
+++ Mod-3/Week-16/create-and-handle-promises/phase-1.js
@@ -1,17 +1,45 @@
 function stretch() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("done stretching")
+      resolve()
+    }, 1000)
+  })
 }
+
+//stretch()
 
 function runOnTreadmill() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("done running on treadmill")
+      resolve()
+    }, 500)
+  })
 }
+
+//runOnTreadmill()
 
 function liftWeights() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("done lifting weights")
+      resolve()
+    }, 2000)
+  })
 }
+
+//liftWeights()
 
 function workout() {
   // Your code here
+    stretch()
+    .then(() => runOnTreadmill())
+    .then(() => liftWeights())
+    .then(() => console.log("done working out"))
 }
 
 
