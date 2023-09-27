6a81abfda20910e22532b2b8a4101e75743eebc6
Victor <viknavarro@gmail.com>
2023-09-27 13:24:35 -0400

    create-remove elements

--- Mod-3/Week-17/element-selection/selectors.js
+++ Mod-3/Week-17/element-selection/selectors.js
@@ -4,22 +4,31 @@ const select = () => {
     /* Section 1 */
     // 1. Get all seeded fruit elements
     // Your code here
-    
+    const seeded = document.getElementsByClassName("seed");
+    console.log(seeded)
     // 2. Get all seedless fruit elements
     // Your code here
-
+    const seedless = document.querySelectorAll(".seedless");
+    console.log(seedless)
     // 3. Get first seedless fruit element
     // Your code here
+    console.log(seedless[0])
 
     /* Section 2 */
     // 4. Get inner span with text "you"
     // Your code here
+    const innerSpan = document.querySelectorAll("span");
+    console.log(innerSpan)
 
     // 5. Get all children of element "wrapper"
     // Your code here
+    const wrapper = document.getElementsByClassName(".wrapper");
+    console.log(wrapper)
 
     // 6. Get all odd number list items in the list
     // Your code here
+    const odd = document.getElementsByClassName("odd");
+    console.log(odd)
 
     // 7. Get all even number list items in the list
     // Your code here
