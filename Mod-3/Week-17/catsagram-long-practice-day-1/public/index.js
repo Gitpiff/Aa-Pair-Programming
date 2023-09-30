3e51fd72c9e1ce26259c4d8ca05fd853638e39fe
Jaden Adams <jadenadams329@gmail.com>
2023-09-30 10:31:02 -0700

    phase 1 complete

--- Mod-3/Week-17/catsagram-long-practice-day-1/public/index.js
+++ Mod-3/Week-17/catsagram-long-practice-day-1/public/index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+
+window.onload = (event) => {
+  const container = document.createElement("div")
+  const title = document.createElement("h1");
+  title.innerText = "Kitten Pic";
+  title.className = "title"
+  document.body.appendChild(title);
+  displayImage();
+
+
+};
+
+
+
+async function getCatPicture() {
+  try {
+    const url = "https://api.thecatapi.com/v1/images/search";
+    const response = await fetch(url);
+    const json = await response.json();
+    // console.log(json)
+    return json
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function displayImage() {
+    const catImage = document.createElement("img")
+    catImage.className = "cat-img"
+    const cat = await getCatPicture()
+    console.log(cat)
+    catImage.setAttribute("src", cat[0].url)
+
+    document.body.appendChild(catImage)
+}
