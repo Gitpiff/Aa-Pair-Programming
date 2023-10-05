b0fab982239e838761b7906c8474c04f1f628b1e
Carmen <carmenshiu07@gmail.com>
2023-10-04 17:53:05 -0700

    phase 2 eventListener in progress

--- Mod-3/Week-18/tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
+++ Mod-3/Week-18/tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
@@ -1,1 +1,28 @@
-// Your code here
+window.onload = function () {
+
+    let playerTurn = "x";
+
+    const boardClass = document.getElementsByClassName('board')
+
+    for (cell of boardClass) {
+        cell.children[0].addEventListener('click', e => {
+
+
+        })
+    }
+
+
+    // const cell = document.getElementById("square-0")
+
+    // cell.addEventListener('click', e => {
+    //     let children = cell.children;
+
+    //     if (children.length === 0) {
+    //         const img = document.createElement("img")
+    //         img.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
+    //         cell.appendChild(img)
+    //     } else {
+    //         return;
+    //     }
+    // })
+}
