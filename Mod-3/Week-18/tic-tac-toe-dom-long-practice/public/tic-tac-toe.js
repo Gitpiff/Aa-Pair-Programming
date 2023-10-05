353c9f3591fdc5ca9e878533a0cca67739825840
Carmen <carmenshiu07@gmail.com>
2023-10-05 15:47:03 -0700

    completed phase 3 + 4

--- Mod-3/Week-18/tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
+++ Mod-3/Week-18/tic-tac-toe-dom-long-practice/public/tic-tac-toe.js
@@ -1,7 +1,35 @@
 window.onload = function () {
 
-    let currPlayer = "x";
+    const h1 = document.querySelector('h1');
+
+    let isGameOver = false;
+    let currPlayer = "X";
     const cells = document.querySelectorAll('.cell');
+
+    const checkGameStatus = () => {
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ]
+
+        for (const combo of winningCombos) {
+            const [a, b, c] = combo;
+
+            if (cells[a].innerHTML &&
+                cells[a].innerHTML === cells[b].innerHTML &&
+                cells[a].innerHTML === cells[c].innerHTML) {
+                h1.innerText = `Winner: ${currPlayer}`
+                isGameOver = true;
+                return
+            }
+        }
+
+        if ([...cells].every(cell => cell.innerHTML)) {
+            h1.innerText = 'Winner: None';
+            isGameOver = true;
+        }
+    }
 
     cells.forEach(cell => {
         cell.addEventListener('click', () =>
@@ -9,19 +37,22 @@ window.onload = function () {
     })
 
     const cellClicks = cell => {
-        if (!cell.innerHTML) {
-            if (currPlayer === 'x') {
+        if (!cell.innerHTML && !isGameOver) {
+            if (currPlayer === 'X') {
                 const x = document.createElement("img")
                 x.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
-                cell.appendChild(x)
-                currPlayer = 'o'
+                cell.appendChild(x);
+                checkGameStatus();
+                currPlayer = 'O';
             } else {
                 const o = document.createElement("img")
                 o.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg"
-                cell.appendChild(o)
-                currPlayer = 'x'
+                cell.appendChild(o);
+                checkGameStatus();
+                currPlayer = 'X';
             }
         }
 
     }
+
 }
