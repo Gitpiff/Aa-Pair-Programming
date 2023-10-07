643c970b8ae00a493a61a50885de663493ec422f
Victor <viknavarro@gmail.com>
2023-10-07 14:05:16 -0600

    assesment pt 2

--- assessments/assessment-week-18-event-handling-storage-practice/your-code.js
+++ assessments/assessment-week-18-event-handling-storage-practice/your-code.js
@@ -1,4 +1,12 @@
 
 window.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const button = document.getElementById("make-circle-blue")
+  button.addEventListener("click",  e => {
+    const circle = document.getElementById("blue-border-circle")
+    circle.className = "blue-fill"
+  })
+
+  const checkMark = document.getElementById("will-not-check")
+  checkMark.addEventListener("click", e => e.preventDefault())
 });
