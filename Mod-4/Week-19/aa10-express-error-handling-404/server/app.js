4242f2dfc411bcd6433c8b13880cd725bc8b5b1b
Victor <viknavarro@gmail.com>
2023-10-12 21:48:44 +0100

    express-route-handlers

--- Mod-4/Week-19/aa10-express-error-handling-404/server/app.js
+++ Mod-4/Week-19/aa10-express-error-handling-404/server/app.js
@@ -5,5 +5,22 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
-const port = 5000;
+//Error Handler
+//Need to create an error before throwing it
+app.use((req, res, next) => {
+  const err = new Error("Resource not found")
+  err.statusCode = 404
+  next(err)
+})
+
+//Catch error and handle it
+app.use((err, req, res, next) => {
+  console.log(err)
+  const status = err.statusCode || 500
+  res.status(status).json({
+    message: err.message || "Something went wrong",
+    status 
+  })
+})
+const port = 9000;
 app.listen(port, () => console.log('Server is listening on port', port));
