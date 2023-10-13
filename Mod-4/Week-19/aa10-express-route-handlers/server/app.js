0725e99c9fb3b4b6cb19da14aed96d9ff25ba36e
Victor <viknavarro@gmail.com>
2023-10-13 22:04:46 +0100

    express-route-handlers

--- Mod-4/Week-19/aa10-express-route-handlers/server/app.js
+++ Mod-4/Week-19/aa10-express-route-handlers/server/app.js
@@ -27,40 +27,48 @@ const app = express();
 // Your code here 
 app.use(express.json())
 
+//Get All Artists
 app.get("/artists", (req, res) => {
   res.json(getAllArtists())
 })
 
+//Get Latest Artist
 app.get("/artists/latest", (req, res) => {
   res.json(getLatestArtist())
 })
 
+//Add Artist
 app.post("/artists", (req, res) => {
   const data = req.body
   res.status(201)
   res.json(addArtist(data))
 })
 
+//Get Albums For Latest Artist
 app.get("/artists/latest/albums", (req, res) => {
   res.json(getAlbumsForLatestArtist())
 })
 
+//Get Artist By ArtistId
 app.get("/artists/:artistId", (req, res) => {
   res.json(getArtistByArtistId(1))
 })
 
+//Edit Artist by ArtistId
 app.put("/artists/:artistId", (req, res) => {
   const artistId = req.params.artistId
   const data = req.body
   res.json(editArtistByArtistId(artistId, data)) //order matters
 })
 
+//Edit Artist by ArtistId
 app.patch("/artists/:artistId", (req, res) => {
   const artistId = req.params.artistId
   const data = req.body
   res.json(editArtistByArtistId(artistId, data)) //order matters
 })
 
+//Delete Artist By ArtistId
 app.delete("/artists/:artistId", (req, res) => {
   const artistId = req.params.artistId
   req.body = {
@@ -70,17 +78,20 @@ app.delete("/artists/:artistId", (req, res) => {
   res.json(deleteArtistByArtistId(artistId))
 })
 
+//Get Albums By ArtistId
 app.get("/artists/:artistId/albums", (req, res) => {
   const artistId = req.params.artistId
   res.json(getAlbumsByArtistId(artistId))
 })
 
+//Add Artist
 app.post("/artists", (req, res) => {
   const data = req.body
   res.status(201)
   res.json(addArtist(data))
 })
 
+//Add Album By ArtistIs
 app.post("/artists/:artistId/albums", (req, res) => {
   const artistId = req.params.artistId
   const data = req.body
@@ -88,8 +99,9 @@ app.post("/artists/:artistId/albums", (req, res) => {
   res.json(addAlbumByArtistId(artistId, data))
 })
 
-app.get("/artists/latest/albums", (req, res) => {
-  res.json(getAlbumsForLatestArtist())
+//Get Album By AlbumId
+app.get("/albums/:albumId", (req, res) => {
+  res.json(getAlbumByAlbumId(1))
 })
 
 app.get("/artists/latest/albums", (req, res) => {
