a92cd264c017ff7cc186bc9c4e1ad7958e5c6989
zack stephens <zxsrhx7@gmail.com>
2023-10-14 12:09:07 -0500

    finished phase 2

--- Mod-4/Week-19/aa10-express-middleware/server/app.js
+++ Mod-4/Week-19/aa10-express-middleware/server/app.js
@@ -3,9 +3,18 @@ const app = express();
 app.use(express.json())
 require("express-async-errors")
 
+const log = (req, res, next) => {
+  res.on('finish', () => {
+    console.log(req.method, req.url, res.statusCode)
+  });
+  next()
+}
+
+app.use(log)
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
+  req.ur
   res.json("Express server running. No content provided at root level. Please use another route.");
 });
 
@@ -13,17 +22,20 @@ app.get('/', (req, res) => {
 app.post('/test-json', (req, res, next) => {
   // send the body as JSON with a Content-Type header of "application/json"
   // finishes the response, res.end()
-  res.json(req.body);
+  res.status(201).json(req.body);
   next();
 });
 
 // For testing express-async-errors
-app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.")
+  res.status(404)
+  next(err)
 });
 
 app.use((err, req, res, next) => {
-  res.json({err: err.message})
+  res.json({ERROR: err.message})
+  console.error(err.message)
 })
 
 const port = 9000;
