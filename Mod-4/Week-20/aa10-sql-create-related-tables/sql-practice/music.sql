60db11428e9e58149fd34d81169c282ce84b14ee
Victor <viknavarro@gmail.com>
2023-10-23 19:00:13 +0100

    sql practice

--- Mod-4/Week-20/aa10-sql-create-related-tables/sql-practice/music.sql
+++ Mod-4/Week-20/aa10-sql-create-related-tables/sql-practice/music.sql
@@ -4,16 +4,31 @@ DROP TABLE IF EXISTS instruments;
 DROP TABLE IF EXISTS musicians;
 DROP TABLE IF EXISTS bands;
 
-CREATE TABLE bands (
+-- Make one to many relationship between the bands and the musicians
+    --//Create a One to Many Relationship
+-- Parent-Child like relationship, where the One is the Parent, and the Many the children
+-- Place the FOREGING KEY into the "Child" table  *2
+-- But before creating the foreging key we must create the column *1
+
+    --//Create a Many to Many
+--To create a musician_instrument table we need an id, and TWO FOREIGN KEYS
+--We need to reference two "Parent" tables to get info about who's musician the instruments belongs to
+--and which intrument it is, to do so we need to have TWO FOREIGN KEYS
+--The FOREIGN KEYS will be musician_id & musician_id
+--They will reference the musician's table at the id and the instruments table also at the id
+
+  -- //Bands have many mnusicians -One to Many- 
+CREATE TABLE bands (    
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(100)
 );
+
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100),
-  band_id INTEGER,
-  FOREIGN KEY (band_id) REFERENCES bands(id) 
+  band_id INTEGER,        --*1
+  FOREIGN KEY (band_id) REFERENCES bands(id) --*2
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
