ad388c7d9c43a11d93e630a931c848f86467d880
Victor <viknavarro@gmail.com>
2023-10-25 01:40:06 +0100

    sql intermediate

--- Mod-4/Week-21/Tuesday/aa11-sql-query-improvement/sql-practice/bonus-improve-sql-queries.sql
+++ Mod-4/Week-21/Tuesday/aa11-sql-query-improvement/sql-practice/bonus-improve-sql-queries.sql
@@ -5,9 +5,13 @@
     -- 55 and belong to a cat that has the color "Olive".
 
     -- Your code here 
+    SELECT COUNT() FROM toys
+    JOIN cat_toys ON toys.id = cat_toys.toy_id
+    JOIN cats ON cats.id = cat_toys.cat_id
+    WHERE toys.price > 55 AND cats.color = 'Olive';
 
 -- Paste your results below (as a comment):
-
+    -- 215
 
 
 
@@ -17,9 +21,17 @@
 -- Query:
 
     -- Your code here 
-
+EXPLAIN QUERY PLAN
+SELECT COUNT() FROM toys
+JOIN cat_toys ON toys.id = cat_toys.toy_id
+JOIN cats ON cats.id = cat_toys.cat_id
+WHERE toys.price > 55 AND cats.color = 'Olive';
 -- Paste your results below (as a comment):
-
+QUERY PLAN
+|--SCAN cat_toys
+|--SEARCH toys USING INTEGER PRIMARY KEY (rowid=?)
+--SEARCH cats USING INTEGER PRIMARY KEY (rowid=?)
+--Run Time: real 0.000 user 0.000071 sys 0.000010
 
 -- What do your results mean?
 
@@ -37,9 +49,12 @@
 -- Query (to be used in the sqlite CLI):
 
     -- Your code here 
-
+SELECT COUNT() FROM toys
+JOIN cat_toys ON toys.id = cat_toys.toy_id
+JOIN cats ON cats.id = cat_toys.cat_id
+WHERE toys.price > 55 AND cats.color = 'Olive';
 -- Paste your results below (as a comment):
-
+--Run Time: real 0.006 user 0.006134 sys 0.000116
 
 
 
@@ -50,12 +65,20 @@
 -- Create index:
 
     -- Your code here 
+    CREATE INDEX idx_cat_toys_cat_id ON cat_toys(cat_id);
+    CREATE INDEX idx_cat_toys_toy_id ON cat_toys(toy_id);
+    CREATE INDEX idx_cat_color ON cats(color);
 
+    EXPLAIN QUERY PLAN
+    SELECT COUNT() FROM toys
+    JOIN cat_toys ON toys.id = cat_toys.toy_id
+    JOIN cats ON cats.id = cat_toys.cat_id
+    WHERE toys.price > 55 AND cats.color = 'Olive';
 -- Analyze Query:
     -- Your code here 
 
 -- Paste your results below (as a comment):
-
+--Run Time: real 0.001 user 0.000079 sys 0.000009
 
 -- Analyze Results:
 
