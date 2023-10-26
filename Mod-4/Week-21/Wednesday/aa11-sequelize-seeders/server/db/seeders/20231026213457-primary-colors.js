b942802880b7a9a74e9aa0bc96bdc4f21aae8572
Victor <viknavarro@gmail.com>
2023-10-26 22:56:28 +0100

    sequelize

---
+++ Mod-4/Week-21/Wednesday/aa11-sequelize-seeders/server/db/seeders/20231026213457-primary-colors.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { Color } = require("../models")    //Import Color Model
+const { Op }  = require("sequelize")
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */ //MODEL            //TABLE   //Columns:values
+   await Color.bulkCreate("Colors", [
+    { name: "red" },
+    { name: "blue" },
+    { name: "yellow" }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(
+      "Colors",
+      {
+        name: {
+          [Op.in]: ["red", "blue", "yellow"]
+        }
+      }, 
+      {}
+    )
+  }
+};
