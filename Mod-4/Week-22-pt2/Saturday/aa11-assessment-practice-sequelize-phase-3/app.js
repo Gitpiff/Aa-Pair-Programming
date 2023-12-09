75679143f2c9d27335015c1956d34377dc0fe296
Victor <viknavarro@gmail.com>
2023-12-09 18:49:06 +0000

    Phase 3 PA done

--- Mod-4/Week-22-pt2/Saturday/aa11-assessment-practice-sequelize-phase-3/app.js
+++ Mod-4/Week-22-pt2/Saturday/aa11-assessment-practice-sequelize-phase-3/app.js
@@ -5,6 +5,47 @@ const app = express();
 
 app.use(express.json());
 
+const { WarehouseItem } = require('./db/models')
+
+app.get('/items', async (req, res, next) => {
+  try {
+    const allItems = await WarehouseItem.findAll({
+      where: {
+        isUsed: false
+      }
+    })
+  
+    return res.json(allItems)
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+})
+
+app.put('/items/:id', async (req, res, next) => {
+  const { id } = req.params
+  const { name, price, quantity, isUsed } = req.body
+  // console.log(req.body.name)
+  const item = await WarehouseItem.findByPk(id)
+  if(item) {
+    const updatedItem = await item.update({
+      name, 
+      price,
+      quantity,
+      isUsed
+    })
+    return res.json(updatedItem)
+  }
+  
+  res.status(404)
+  res.json(
+    {
+      message: "Warehouse Item not found"
+    }
+  )
+})
+
 if (require.main === module) {
   const port = 8003;
   app.listen(port, () => console.log('Server-3 is listening on port', port));
