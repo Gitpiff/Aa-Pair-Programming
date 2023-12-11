1802fef7b57d0f185f7276e6c7b1a078b4b74df4
Victor <viknavarro@gmail.com>
2023-12-11 20:54:56 +0000

    Sequelize PT 3

--- Mod-4/Week-23/Monday/aa11-assessment-version-a-sequelize-phase-1/scratch.js
+++ Mod-4/Week-23/Monday/aa11-assessment-version-a-sequelize-phase-1/scratch.js
@@ -4,9 +4,10 @@
 //To generate a seeder
         //npx sequelize seeder:generate --name 
 
-//To seed data you need to Migrate First
+//To seed data you need to Migrate your data First
+//To run a migration
     //npx dotenv sequelize db:migrate
 
-//Seed data
+//To Seed data
     //npx dotenv sequelize db:seed      //If only want to seed one file
     //npx dotenv sequelize db:seed:all  //If want to seed ALL seeder files
--- Mod-4/Week-23/Monday/aa11-assessment-version-a-sequelize-phase-3/routes/entrees.js
+++ Mod-4/Week-23/Monday/aa11-assessment-version-a-sequelize-phase-3/routes/entrees.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 // Your code here 
 
+const { Entree } = require('../db/models')
+
+router.get('/', async (req, res) => {
+    const entrees = await Entree.findAll({
+        order: [['price', 'DESC'], ['name']]
+    })
+    console.log(entrees)
+    return res.json(entrees)
+})
+
 module.exports = router;
