25362bfa91a3ee463436707b122a08fd608343b2
Victor <viknavarro@gmail.com>
2023-12-31 00:15:14 +0000

    PA pagination

--- Practice_Assessments/assessment-week-24/aa12-assessment-practice-sequelize-search-filter-pagination/routes/players.js
+++ Practice_Assessments/assessment-week-24/aa12-assessment-practice-sequelize-search-filter-pagination/routes/players.js
@@ -9,9 +9,25 @@ router.get('/', async (req, res) => {
   const where = {};
   const query = {};
 
-  //Page HAS to be a number
-  //Ig page is not a number OR is less than 0, default it to 1, else parse the value of page into a num
+  //Page and Size HAVE to be numbers 
+  //If page is not a number OR is less than 0, default it to 1, else parse the value of page into a num
   page = isNaN(page) || page < 0 ? 1 : parseInt(page); 
+  // if(isNaN(page) || page <= 0) {
+  //   page = 1;
+  // } else {
+  //   page = parseInt(page);
+  // };
+  size = isNaN(size) || size < 0 ? 2 : parseInt(size);
+  // if(isNaN(size) || size <= 0) {
+  //   size = 2;
+  // } else {
+  //   size = parseInt(size);
+  // };
+  if(size > 10) size = 10;
+
+  //In SQL page becomes limit and size becomes offset
+  query.limit = size;
+  query.offset = size * (page - 1); //How many items we 'skip'
 
   if (firstName && firstName !== '') {
     where.firstName = firstName[0].toUpperCase() + firstName.slice(1);
@@ -39,9 +55,12 @@ router.get('/', async (req, res) => {
 
   const players = await Player.findAll({
     where,
+    ...query
   });
   return res.json({
     players,
+    page,
+    size
   });
 });
 
