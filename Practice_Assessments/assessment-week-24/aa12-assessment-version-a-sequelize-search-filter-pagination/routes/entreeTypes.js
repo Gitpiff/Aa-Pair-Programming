df43b11ca1bb1826c3cdf7dbca58775c15496459
Victor <viknavarro@gmail.com>
2024-01-03 00:41:35 +0000

    Sequelize filter pagination

--- Practice_Assessments/assessment-week-24/aa12-assessment-version-a-sequelize-search-filter-pagination/routes/entreeTypes.js
+++ Practice_Assessments/assessment-week-24/aa12-assessment-version-a-sequelize-search-filter-pagination/routes/entreeTypes.js
@@ -4,6 +4,27 @@ const router = express.Router();
 const { EntreeType } = require('../db/models');
 
 router.get('/', async (req, res) => {
+  const { isVegetarian } = req.query;
+
+  const where = {};
+  
+  if(!isVegetarian) {
+    const entreeTypes = await EntreeType.findAll();
+    return res.json(entreeTypes);
+  }
+
+  if(isVegetarian === "false") {
+    where.isVegetarian = false
+  } else if(isVegetarian === "true") {
+    where.isVegetarian = true
+  } else if(isVegetarian !== null) {
+    res.status(400);
+  return res.json({
+     errors: [
+       { message: 'isVegetarian should be either true or false' }
+     ]
+  });
+  }
   // Example error response for incorrect isVegetarian search filter:
   // res.status(400);
   // return res.json({
@@ -12,7 +33,9 @@ router.get('/', async (req, res) => {
   //    ]
   // });
 
-  const entreeTypes = await EntreeType.findAll();
+  const entreeTypes = await EntreeType.findAll({
+    where
+  });
 
   return res.json(entreeTypes);
 });
--- Practice_Assessments/assessment-week-24/aa12-assessment-version-a-sequelize-search-filter-pagination/routes/ingredients.js
+++ Practice_Assessments/assessment-week-24/aa12-assessment-version-a-sequelize-search-filter-pagination/routes/ingredients.js
@@ -6,14 +6,17 @@ const { Ingredient } = require('../db/models');
 router.get('/', async (req, res) => {
   let { page, size } = req.query;
 
-  page = parseInt(page);
-  size = parseInt(size);
+  // page = parseInt(page);
+  // size = parseInt(size);
 
-  if (Number.isNaN(page)) page = 1;
-  if (Number.isNaN(size)) size = 4;
+  page = isNaN(page) || page <= 0 ? 1 : parseInt(page);
+  size = isNaN(size) || size <= 0 ? 4 : parseInt(size);
+
+  // if (Number.isNaN(page)) page = 1;
+  // if (Number.isNaN(size)) size = 4;
 
   const ingredients = await Ingredient.findAll({
-    limit: page,
+    limit: size,
     offset: size * (page - 1),
   });
 
