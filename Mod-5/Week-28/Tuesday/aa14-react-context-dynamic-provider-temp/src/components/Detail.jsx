d6a0670c87b03b2676db6186579515a43cc56e95
Victor <viknavarro@gmail.com>
2024-01-24 00:12:37 +0000

    react context dynamic provider practice done

--- Mod-5/Week-28/Tuesday/aa14-react-context-dynamic-provider-temp/src/components/Detail.jsx
+++ Mod-5/Week-28/Tuesday/aa14-react-context-dynamic-provider-temp/src/components/Detail.jsx
@@ -2,7 +2,8 @@ import { useContext } from 'react';
 import { HoroscopeContext } from '../context/HoroscopeContext';
 
 const Detail = () => {
-  const horoscopesObj = useContext(HoroscopeContext);
+  //const horoscopesObj = useContext(HoroscopeContext);
+  const { sign } = useContext(HoroscopeContext);
 
   return (
     <div className='details'>
@@ -10,9 +11,9 @@ const Detail = () => {
         src='https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg'
         alt=''
       />
-      <h2>Current Sign Name</h2>
-      <h4>Element: </h4>
-      <h4>Traits: </h4>
+      <h2>{sign.name}</h2>
+      <h4>{sign.element}</h4>
+      <h4>{sign.traits}</h4>
     </div>
   );
 };
