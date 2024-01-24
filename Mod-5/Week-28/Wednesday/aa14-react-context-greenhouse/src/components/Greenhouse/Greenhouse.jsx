59e7cb3e5f7549b13e26974913c0af0f870c7cf6
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-01-24 18:38:05 -0500

    Phase 1

--- Mod-5/Week-28/Wednesday/aa14-react-context-greenhouse/src/components/Greenhouse/Greenhouse.jsx
+++ Mod-5/Week-28/Wednesday/aa14-react-context-greenhouse/src/components/Greenhouse/Greenhouse.jsx
@@ -1,17 +1,19 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
-
+ const { themeName } = useTheme();
+console.log(themeName, "THEME NAME")
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+            src={themeName === 'day' ? dayImage: nightImage}
+            alt='greenhouse'
       />
       <LightSwitch />
       <ClimateStats />
