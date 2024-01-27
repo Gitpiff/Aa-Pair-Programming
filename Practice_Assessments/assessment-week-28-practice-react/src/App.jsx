5fed28107cd314148f6d86102ba03bba22c84a6f
Victor <viknavarro@gmail.com>
2024-01-27 17:41:59 +0000

    PA React pt 1 and 2 done

--- Practice_Assessments/assessment-week-28-practice-react/src/App.jsx
+++ Practice_Assessments/assessment-week-28-practice-react/src/App.jsx
@@ -1,7 +1,50 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import FruitsIndex from './components/FruitsIndex';
+import FruitShow from './components/FruitShow';
+import FruitForm from './components/FruitForm';
+import FavoriteFruit from './components/FavoriteFruit';
+import SetFavoriteFruit from './components/SetFavoriteFruit';
+import Navigation from './components/Navigation';
+
 import fruits from './mockData/fruits.json';
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <FruitsIndex fruits={fruits}/>
+    },
+    {
+      path: '/fruits/:fruitId',
+      element: <FruitShow fruits={fruits}/>
+    },
+    {
+      path: '/fruits/new',
+      element: <FruitForm fruits={fruits}/>
+    },
+    {
+      path: '/fav-fruit',
+      element: <FavoriteFruit fruits={fruits}/>
+    },
+    {
+      path: '/set-fav-fruit',
+      element: <SetFavoriteFruit fruits={fruits}/>
+    },
+    {
+      path: '*',
+      element: <h1>Page Not Found</h1>
+    }
+  ]
+)
+
 function App() {
-  return <h1>Welcome to Fruits App</h1>;
+  return (
+    <>
+    <h1>Fruits App</h1>
+    <Navigation />
+     <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App;
--- Practice_Assessments/assessment-week-28-practice-react/src/components/Navigation.jsx
+++ Practice_Assessments/assessment-week-28-practice-react/src/components/Navigation.jsx
@@ -1,5 +1,34 @@
+import { NavLink } from "react-router-dom";
+
 function Navigation() {
-  return null;
+  
+  return (
+ 
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/fruits/new'>
+              Enter a Fruit
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/fav-fruit'>
+               Favorite Fruit
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/set-fav-fruit'>
+              Set Favorite Fruit
+             </NavLink>
+          </li>
+        </ul>
+      </nav>
+  );
 }
 
 export default Navigation;
