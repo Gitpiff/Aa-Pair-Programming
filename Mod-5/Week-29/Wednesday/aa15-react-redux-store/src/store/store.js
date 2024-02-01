be2fec040e2fe478e5f676bcd6b9019cb9a46211
Victor <viknavarro@gmail.com>
2024-02-01 00:52:44 +0000

    react-redux-store pt1 done

--- Mod-5/Week-29/Wednesday/aa15-react-redux-store/src/store/store.js
+++ Mod-5/Week-29/Wednesday/aa15-react-redux-store/src/store/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import fruitReducer from './fruitReducer';
 
 /*
 `combineReducers` combines all the reducer functions into one big reducer
@@ -9,6 +10,7 @@ components.
 
 // create rootReducer here:
 // Your code here 
+const rootReducer = combineReducers({ fruitState: fruitReducer })
 
 /*
 `enhancer` allows you to alter the store and add functionality such as Redux
