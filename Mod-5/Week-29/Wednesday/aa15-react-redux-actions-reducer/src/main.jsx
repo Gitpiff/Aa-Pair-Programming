32781f290c7cf162dcff9a98f9c9c348460aa17f
Victor <viknavarro@gmail.com>
2024-02-01 01:38:10 +0000

    react-redux

--- Mod-5/Week-29/Wednesday/aa15-react-redux-actions-reducer/src/main.jsx
+++ Mod-5/Week-29/Wednesday/aa15-react-redux-actions-reducer/src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
+import { loadArticles } from './store/articleReducer';
 import './index.css';
 import configureStore from './store';
 
 const store = configureStore();
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;
+  window.loadArticles = loadArticles;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
