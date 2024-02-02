052565399a100ba563ec4f13fd00bf3f985d72a5
Victor <viknavarro@gmail.com>
2024-02-02 01:03:33 +0000

    redux forms practice

--- Mod-5/Week-29/Thursday/aa15-react-redux-parameters/src/components/SingleArticle/SingleArticle.jsx
+++ Mod-5/Week-29/Thursday/aa15-react-redux-parameters/src/components/SingleArticle/SingleArticle.jsx
@@ -5,21 +5,22 @@ import './SingleArticle.css';
 const SingleArticle = () => {
   //get the id
   let { id } = useParams();
-  //load ALL of the articles
+  //load ALL of the articles from the store
   const articles = useSelector((state) => state.articleState.entries)
   //find current article
   const currentArticle = articles.find(article => article.id === id)
   console.log(currentArticle)
-  
+
   return (
     <div className='singleArticle'>
-      <h1>{currentArticle.title}</h1>
+      {/* the question mark checks if the article exists then render the title, else skip it */}
+      <h1>{currentArticle?.title}</h1>
       <img
-        src={currentArticle.imageUrl}
+        src={currentArticle?.imageUrl}
         alt='home'
       />
       <p>
-        {currentArticle.body}
+        {currentArticle?.body}  
       </p>
     </div>
   );
