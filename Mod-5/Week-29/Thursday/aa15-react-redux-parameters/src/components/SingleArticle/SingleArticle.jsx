fae698fb404ce3399e95ede59182910b81bb19e3
Victor <viknavarro@gmail.com>
2024-02-02 00:15:44 +0000

    redux parameters practice done

--- Mod-5/Week-29/Thursday/aa15-react-redux-parameters/src/components/SingleArticle/SingleArticle.jsx
+++ Mod-5/Week-29/Thursday/aa15-react-redux-parameters/src/components/SingleArticle/SingleArticle.jsx
@@ -1,18 +1,25 @@
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './SingleArticle.css';
 
 const SingleArticle = () => {
+  //get the id
+  let { id } = useParams();
+  //load ALL of the articles
+  const articles = useSelector((state) => state.articleState.entries)
+  //find current article
+  const currentArticle = articles.find(article => article.id === id)
+  console.log(currentArticle)
+  
   return (
     <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
+      <h1>{currentArticle.title}</h1>
       <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
+        src={currentArticle.imageUrl}
         alt='home'
       />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
+        {currentArticle.body}
       </p>
     </div>
   );
