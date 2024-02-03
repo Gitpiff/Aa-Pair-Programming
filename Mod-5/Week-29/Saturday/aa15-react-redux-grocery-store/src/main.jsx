b383776d333fec581febfe90cbb5963b2882b624
Victor <viknavarro@gmail.com>
2024-02-03 18:18:25 +0000

    store pt 1 done

--- Mod-5/Week-29/Saturday/aa15-react-redux-grocery-store/src/main.jsx
+++ Mod-5/Week-29/Saturday/aa15-react-redux-grocery-store/src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
 import App from './App';
 import './index.css';
 
+// (a preloadedState does not need to be passed in, it can be undefined).
+const store = configureStore();
+
+// Expose the store on the window ONLY in development for testing purposes.
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ Mod-5/Week-29/Saturday/aa15-react-redux-grocery-store/src/store/store.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+
+// createStore - creates a Redux store
+// combineReducers - creates one reducer from multiple reducers as slices of state
+// applyMiddleware - a store enhancer that will allow you to attach middlewares (a middleware is a function called before any action hits the root reducer)
+// compose - another store enhancer that will allow you to use more than one store enhancer
+
+// rootReducer which will be the root reducer for the Redux store. The rootReducer will just be the return of combineReducers invoked with an empty object for now.
+const rootReducer = combineReducers({
+
+});
+
+
+let enhancer;
+
+if (import.meta.env.MODE !== "production") {
+    const logger = (await import("redux-logger")).default;
+    const composeEnhancers =
+      typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true })
+        : compose;
+    enhancer = composeEnhancers(applyMiddleware(logger));
+  }
+
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer)
+};
+
+export default configureStore;
