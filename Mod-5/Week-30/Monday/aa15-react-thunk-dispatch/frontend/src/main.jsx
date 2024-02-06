365df72a1c849f10ca09bae677eafc1751ed9146
Victor <viknavarro@gmail.com>
2024-02-06 21:15:02 +0000

    react-thunk practice

--- Mod-5/Week-30/Monday/aa15-react-thunk-dispatch/frontend/src/main.jsx
+++ Mod-5/Week-30/Monday/aa15-react-thunk-dispatch/frontend/src/main.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as articleActions from './store/articleReducer';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import * as articleActions from './store/articleReducer';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
   window.store = store;
-  window.articleActions = articleActions
+  window.articleActions = articleActions;
 }
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- Mod-5/Week-30/Monday/aa15-react-thunk-dispatch/frontend/src/store/articleReducer.js
+++ Mod-5/Week-30/Monday/aa15-react-thunk-dispatch/frontend/src/store/articleReducer.js
@@ -1,7 +1,5 @@
-
 const LOAD_ARTICLES = 'article/loadArticles';
 const ADD_ARTICLE = 'article/addArticle';
-
 
 export const loadArticles = (articles) => {
   return {
@@ -17,26 +15,26 @@ export const addArticle = (article) => {
   };
 };
 
-//Create a thunk
 export const fetchArticles = () => async (dispatch) => {
-  const response = await fetch("/api/articles");
+  const response = await fetch('/api/articles');
   const articles = await response.json();
-  dispatch(loadArticles(articles))
-}
+  dispatch(loadArticles(articles));
+};
 
 export const writeArticle = (payload) => async (dispatch) => {
-  const res = await fetch("/api/articles", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(payload)
-  })
-  if (res.ok) {
-    const article = await res.json();
-    dispatch(addArticle(article))
+  const response = await fetch('/api/articles', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  });
+
+  if (response.ok) {
+    const article = await response.json();
+    dispatch(addArticle(article));
+    return article;
   }
-}
+};
+
 const initialState = { entries: [], isLoading: true };
 
 const articleReducer = (state = initialState, action) => {
