53f6cd2857ae40f1c9da4e49ac7b10fca3788e85
Victor <viknavarro@gmail.com>
2024-02-28 02:00:32 +0000

    phyton basics

---
+++ Mod-6/Tuesday/input.py
@@ -0,0 +1,26 @@
+# Simple Input and Formatted Printing
+# - Prompt for input()
+# - Formatted printing 4 ways
+
+# name = input("What is your name?\n")
+# print(name)
+
+# print("Hi, " + name + ".")
+
+# print("Hi, %s." % name)
+
+# print("Hi, {0}.".format(name))
+
+# print(f'Hi, {name}.')
+
+
+
+# Arithmetic with Strings
+
+a = "a"
+b = "b"
+an = "an"
+
+print(b + an)
+print(b + a*7)
+print(b + an*2 + a)
