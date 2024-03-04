43ebd8ae614da227c616c377dc2f099ed88183fc
Victor <viknavarro@gmail.com>
2024-03-04 22:40:33 +0000

    tuples phyton

--- Mod-6/Week-33/Saturday/tuples.py
+++ Mod-6/Week-33/Saturday/tuples.py
@@ -1,10 +1,49 @@
 # Tuples
-# - declare
+# - Immutable
+# - Iterable
+# - declare -can be delcared without parentheses as long as they contain multiple elements separated by a comma
 # - cannot append, remove or sort in place
 # - sorted()
 # - min, max, sum, len
 # - empty and single item tuples
 # - returning tuples from functions
 
-a = (1, 2, 3, 4, 5, 6, 7)
-b = ('a', 'b', 'c')
+# a = (1, 2, 3, 4, 5, 6, 7)
+# b = ('a', 'b', 'c')
+# c = 10, 20, 30
+
+# shopping = ('pan','apples', 'leche')
+# alphaShopping = sorted(shopping)
+# print(alphaShopping)
+
+# shoppingStops = (
+#     ['pan', 'leche', 'juevos'], 
+#     ['futbol'],
+# )
+
+# print(shoppingStops)
+# shoppingStops[0].append('butter')
+# print(shoppingStops)
+
+# users = [
+#     (1, 'user_a'),
+#     (2, 'user_b'),    
+# ]
+
+# print(users)
+
+# scores = (15, 66, 34, 39, 54, 12)
+# print(max(scores))
+# print(min(scores))
+# print(sum(scores) / len(scores))
+# print(sorted(scores))
+
+# def minmax(numbers):
+#     return min(numbers), max(numbers)
+
+# myNums = (7, 35, 25, 9, 11)
+# print(minmax(myNums))
+
+# (lowest, highest) = minmax(myNums)
+# print(lowest)
+# print(highest)
