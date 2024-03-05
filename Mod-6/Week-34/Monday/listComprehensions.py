3aa3e8bbc73ba1e8a95e27544015354ced18ad21
Victor <viknavarro@gmail.com>
2024-03-05 02:07:49 +0000

    processing lists phyton

---
+++ Mod-6/Week-34/Monday/listComprehensions.py
@@ -0,0 +1,100 @@
+# -Handle Iterables
+# -Similar to JS arrow functions map/filter
+# -Two components at a minimum [value that we want to add to the list, for-loop]
+# -Can do more than one loop at the time
+
+lst = [1, '2', 'three', True, None]
+# TRADITIONAL FOR LOOP
+new_lst = []
+for item in lst:
+    new_lst.append(item)
+
+
+# LIST COMPREHENSION
+lst_cmpre = [item for item in lst]
+# print(lst_cmpre)
+
+# EXAMPLE, TITLE CASE THE FOLLOWING LIST
+list2 = ['jerry', 'MARY', 'carrIE', 'larry']
+
+# WITH A TRADITIONAL FOR LOOP
+title_list = []
+for name in list2:
+    title_list.append(name.title())
+
+# print(title_list)
+
+# LIST COMPREHENSION
+title_lst_cmpre = [name.title() for name in list2]
+
+# print(title_lst_cmpre)
+
+
+
+# **************** FILTER *******************
+# list comprehensive => [value, for-loop, condition]
+
+num_list = [1, 20, 38, 7, 9, 65, 35, 17, 33]
+
+# FOR LOOP
+new_num_list = []
+for num in num_list:
+    if num % 3 == 0:
+        new_num_list.append(num)
+
+# print(new_num_list)
+
+# LIST COMPREHENSIVE
+compre_num_list = [num for num in num_list if num % 5 ==0]
+# print(compre_num_list)
+
+
+
+# **************** TUPLES *******************
+# Get the following tuples (1, 'a'), (1, 'b'), (1, 'c'), etc.
+letters = ['a', 'b', 'c']
+nums = [1, 2, 3]
+
+# FOR LOOP
+new_tuple_list = []
+for letter in letters:
+    for num in nums:
+        new_tuple_list.append((num, letter))
+
+# print(new_tuple_list)
+
+# LIST COMPREHENSIVE
+# [value, outer-for-loop, inner-for-loop]
+compre_tuple_list = [(num, letter) for num in nums for letter in letters]
+
+# print(compre_tuple_list)
+
+
+
+# 
+# **************** DICTIONARY COMPREHENSION *******************
+# - just stores values
+# - can't have any methods
+
+keys = ['age', 'name', 'height']
+values = [32, 'Corina', 1.4]
+
+# FOR LOOP
+d = dict()
+for i in range(len(keys)):
+    d[keys[i].title()] = values[i]
+
+print(d)
+
+# DICTIONARY COMPREHENSIVE
+# { key: value, for-loop }
+
+compre_d = { keys[i].title(): values[i] for i in range(len(keys))}
+
+print(compre_d)
+
+
+# USING ZIP -PREFERED WAY-
+zip_d = { key.title(): value for key, value in zip(keys, values)}
+
+print(zip_d)
--- Mod-6/Week-34/Monday/processingLists.py
+++ Mod-6/Week-34/Monday/processingLists.py
@@ -5,12 +5,79 @@
 # - zip
 # - custom sort
 
-users = [
-    {'id': 12323, 'displayName': 'Jose Smith', 'email': 'joe.smith@here.com'},
-    {'id': 12324, 'displayName': 'andres perez', 'email': 'andres.perez@here.com'},
-]
+# users = [
+#     {'id': 12323, 'displayName': 'Jose Smith', 'email': 'joe.smith@here.com'},
+#     {'id': 12324, 'displayName': 'andres perez', 'email': 'andres.perez@here.com'},
+# ]
 
-print(users)
+# print(users)
 
-def sorter(user):
-    return item['displayName']
+# def sorter(user):
+#     return item['displayName'].lower()
+
+# users.sort(key=sorter)
+# print(users)
+
+# reversedUsers = sorted(users, key=sorter, reverse=True)
+# print(reversedUsers)
+
+# ALL AND ANY
+
+# print("all() - any item that is false") #> if any list has an empty item returns False
+# titles1 = ['Mr', 'Mrs', 'Ms']
+# titles2 = ['Mr', 'Mrs', 'Ms', '']
+# titles3 = []
+# print(all(titles1)) #>Checks if every item has value, returns True or False
+# print(all(titles2))
+# print(all(titles3))
+
+
+# print("any()") #> if all items empty/not content return False
+# feedback1 = ['', '', '']
+# feedback2 = ['Fun!', '', 'Clean!']
+# feedback3 = []
+# print(any(feedback1), feedback1)
+# print(any(feedback2), feedback2)
+# print(any(feedback3), feedback3)
+
+# Adopted Cats
+# Given a list of Cat objects (dictionaries representing cats), write a function
+# cat_verify that uses the all() built-in function to determine if all cats
+# are the same breed. Then use any() to determine if any of them are up for
+# adoption. Return the result as a tuple.
+
+
+# The breed represents the cat's breed, and adopted represents whether the
+# cat bas been adopted already or not.
+
+# Write your code here.
+
+# FILTER AND MAP
+
+scores = [90, 86, 75, 91, 62, 99, 54, 70]
+
+def isA(num):
+    return num >= 90
+
+aScores = filter(isA, scores)
+print(scores)
+print(list(aScores))
+
+def getGrade(num):
+    if num >= 90:
+        return 'A'
+    elif num < 90 and num >= 80:
+        return 'B'
+    elif num < 80 and num >= 70:
+        return 'C'
+    elif num < 70 and num >= 60:
+        return 'D'
+    else: 
+        return 'F'
+
+grades = list(map(getGrade, scores))
+print(grades)
+
+print('ZIPPED GRADES AND SCORES')
+combined = list(zip(scores, grades))
+print(combined)
