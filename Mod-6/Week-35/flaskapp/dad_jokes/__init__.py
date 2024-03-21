8683e4d5a83552b3762969d2302714d313543da2
Victor <viknavarro@gmail.com>
2024-03-21 23:05:49 +0000

    dad jokes flask created template displaying random joke

--- Mod-6/Week-35/flaskapp/dad_jokes/__init__.py
+++ Mod-6/Week-35/flaskapp/dad_jokes/__init__.py
@@ -1,4 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from .config import Config
+from .db_jokes import dad_jokes
+from random import choice
 
 app = Flask(__name__)
-print("dunder name", __name__)
+#print("dunder name", __name__)
+app.config.from_object(Config)
+
+# Defaults to a GET route, if we want to add a different method - @app.route("/", methods=["POST"]) 
+@app.route("/")
+def index():
+    # this is where you ad DB functionality
+    joke = choice(dad_jokes)
+    print(joke)
+    return render_template("index.html", joke=joke)
---
+++ Mod-6/Week-35/flaskapp/dad_jokes/db_jokes.py
@@ -0,0 +1,35 @@
+
+dad_jokes = [
+    {
+        "jokeid": 1,
+        "joke": "I'm reading a book on the history of glue.",
+        "punchline": "I just can't seem to put it down.",
+        "rating": 8
+    },
+    {
+        "jokeid": 2,
+        "joke": "I used to play piano by ear, but now I use my hands.",
+        "punchline": "It sounds better.",
+        "rating": 7
+    },
+    {
+        "jokeid": 3,
+        "joke": "Why couldn’t the bicycle stand up by itself?",
+        "punchline": "It was two tired.",
+        "rating": 9
+    },
+    {
+        "jokeid": 4,
+        "joke": "I'm reading a book on anti-gravity.",
+        "punchline": "It's impossible to put down.",
+        "rating": 8.5
+    },
+    {
+        "jokeid": 5,
+        "joke": "Did you hear about the mathematician who’s afraid of negative numbers?",
+        "punchline": "He’ll stop at nothing to avoid them.",
+        "rating": 7.5
+    }
+]
+
+dad_jokes
